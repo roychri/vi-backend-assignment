4bd07e6359ca5d22af708c789669585cb264990c
Christian Roy <christian@roy.name>
2022-08-26 18:14:31 -0400

    Make eslint happy

--- test/index.test.js
+++ test/index.test.js
@@ -1,44 +1,44 @@
-const request = require('supertest')
-const server = require('../src')
+const request = require( "supertest" );
+const server = require( "../src" );
 
-afterAll(async () => {
+afterAll( async () => {
     await server.close();
 });
 
-describe('Get /', () => {
+describe( "Get /", () => {
     beforeEach( async function() {
-        this.res = await request(server)
-            .get('/');
+        this.res = await request( server )
+            .get( "/" );
     });
-    it('should succeed', async function () {
-        expect(this.res.statusCode).toEqual(200)
+    it( "should succeed", async function () {
+        expect( this.res.statusCode ).toEqual( 200 );
     });
 });
 
-describe('Get /moviesPerActor', () => {
+describe( "Get /moviesPerActor", () => {
     beforeEach( async function() {
-        this.res = await request(server)
-            .get('/moviesPerActor');
+        this.res = await request( server )
+            .get( "/moviesPerActor" );
     });
-    it('should succeed', async function () {
-        expect(this.res.statusCode).toEqual(200)
+    it( "should succeed", async function () {
+        expect( this.res.statusCode ).toEqual( 200 );
     });
-    it('should return an object', async function () {
-        expect(typeof this.res.body).toBe('object');
+    it( "should return an object", async function () {
+        expect( typeof this.res.body ).toBe( "object" );
     });
-    describe( 'Each Actor', function() {
-        it('should be an array of strings', async function () {
+    describe( "Each Actor", function() {
+        it( "should be an array of strings", async function () {
             const keys = Object.keys( this.res.body );
-            expect( Array.isArray( this.res.body[keys[0]]) ).toBe( true );
+            expect( Array.isArray( this.res.body[keys[0]] ) ).toBe( true );
         });
     });
 });
 
-describe('Get /actorsWithMultipleCharacters', () => {
-    it('should return an object', async function() {
-        const res = await request(server)
-            .get('/actorsWithMultipleCharacters');
-        expect(res.statusCode).toEqual(200)
-        expect(typeof res.body).toBe('object');
+describe( "Get /actorsWithMultipleCharacters", () => {
+    it( "should return an object", async function() {
+        const res = await request( server )
+            .get( "/actorsWithMultipleCharacters" );
+        expect( res.statusCode ).toEqual( 200 );
+        expect( typeof res.body ).toBe( "object" );
     });
 });
